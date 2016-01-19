7d99556631851d6f6b290a2222f77833fdf48f6a
enwint <enwinters@comcast.net>
2016-01-18 22:00:28 -0500

    populating board method
    only pushing so i can pull, not final

--- src/edu/virginia/bookmark/Board.java
+++ src/edu/virginia/bookmark/Board.java
@@ -15,11 +15,48 @@ public class Board {
 	}
 	
 	public void fillBoard() {
-		// Fills all the slots in the board with cards
-		// that are randomly sampled from the class decks
+		ArrayList<Card> classCards = new ArrayList<Card>();
+
+		classCards = getClassDeck();
+
+		ArrayList<Integer> usedNums = new ArrayList<Integer>();
+
+		Random rand = new Random();
+			int index = 0;
+
+		while(counter < BOARD_HEIGHT*BOARD_WIDTH) {
+			index = rand.nextInt(BOARD_WIDTH*BOARD_HEIGHT);
+			if(!usedNums.contains(index)) {
+				board[i][j] = classCards.get(index);
+				
+			}
+
+			else {
+
+				usedNums.add(index);
+			}
+		}
+
+
 	}
 
 	public Card returnCardAtPos(Point pos) {
 		return board[pos.x][pos.y];
 	}
+
+	/* public static HashMap<String, Object> getBoardPropertiesFromXMLElement(Element boardData) {
+
+		String xmlBodyText = XMLHelper.getTextValue(cardData, "team");
+		int xmlTurnId = XMLHelper.getIntValue(cardData, "turn_id");
+	
+		
+		HashMap<String, Object> properties = new HashMap<String, Object>();
+
+		properties.put("type", xmlTypeStr);
+		properties.put("bodyText", xmlBodyText);
+		properties.put("pageStart", xmlPageStart);
+		properties.put("pageEnd", xmlPageEnd);
+		
+		return properties;
+	} */
 }
